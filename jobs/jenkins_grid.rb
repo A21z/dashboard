1118d66dae38daf0ff72c861ef19d8582e4bf932
Christian Rasp <christian.rasp@commercetools.de>
2014-04-02 13:11:47 +0200

    Remove unused stuff. Cleanup ruby file.

--- jobs/jenkins_grid.rb
+++ jobs/jenkins_grid.rb
@@ -2,13 +2,9 @@ require 'net/http'
 require 'json'
 
 url = 'http://hub.ci.cloud.commercetools.de/'
-seconds_since_last_build_failed = 0
-seconds_last_build_record = 0
-is_any_build_failing = false
 
 views = {
   grid_base: { name: 'grid-base', prio: 1 },
-  grid_ext: { name: 'grid-ext', prio: 1 },
   grid_solr: { name: 'grid-solr', prio: 2 },
   grid_stores: { name: 'grid-stores', prio: 1 },
   grid_webtests: { name: 'grid-webtests', prio: 2 },
@@ -16,7 +12,7 @@ views = {
 
 
 # Collect failing jobs
-SCHEDULER.every '6s', :first_in => 0 do
+SCHEDULER.every '10s', :first_in => 0 do
   
   uri      = URI.parse(url)
   http     = Net::HTTP.new(uri.host, uri.port)
@@ -31,7 +27,34 @@ SCHEDULER.every '6s', :first_in => 0 do
   jobs_failed.map! { |job|
     { name: trim_job_name(job['name']), state: job['color'] }
   }
+  jobs_building = jobs.select { |job|
+    (job['color'].include? 'anime')
+  }
+  jobs_building.map! { |job|
+    { name: trim_job_name(job['name']), state: job['color'] }
+  }
 
+  send_event('jenkins_jobs_grid_failed', { jobs: jobs_failed })
+  send_event('jenkins_build', { jobs: jobs_building })
+end
+
+
+# Collect failing jobs
+SCHEDULER.every '10s', :first_in => 0 do
+
+  uri      = URI.parse(url)
+  http     = Net::HTTP.new(uri.host, uri.port)
+  api_url  = url + '/api/json?tree=jobs[name,color]'
+  response = http.request(Net::HTTP::Get.new(api_url))
+  jobs     = JSON.parse(response.body)['jobs']
+
+  # Filter, what we want to see
+  jobs_failed = jobs.select { |job|
+    (!job['color'].include? 'blue') && (!job['name'].include? 'sphere')
+  }
+  jobs_failed.map! { |job|
+    { name: trim_job_name(job['name']), state: job['color'] }
+  }
   jobs_building = jobs.select { |job|
     (job['color'].include? 'anime')
   }
@@ -45,7 +68,7 @@ end
 
 
 # Collect jobs, that are currently in the queue
-SCHEDULER.every '6s', :first_in => 0 do
+SCHEDULER.every '5s', :first_in => 0 do
 
   uri      = URI.parse(url)
   http     = Net::HTTP.new(uri.host, uri.port)
@@ -64,7 +87,7 @@ end
 
 
 # Collect job group status
-SCHEDULER.every '6s', :first_in => 0 do
+SCHEDULER.every '5s', :first_in => 0 do
 
   views.each do |key, view|
     critical_count = 0
@@ -84,9 +107,11 @@ SCHEDULER.every '6s', :first_in => 0 do
       end
     end
     
-    status = critical_count > 0 ? "danger" : (warning_count > 0 ? "warning" : "ok")
-
-    is_any_build_failing = "ok" == status ? false : true;
+    status = if critical_count > 0 then
+               'danger'
+             else
+               warning_count > 0 ? 'warning' : 'ok'
+             end
 
     send_event('jenkins_status_view_' + key.to_s, { criticals: critical_count, warnings: warning_count, status: status, prio: view[:prio]})
   end
@@ -94,7 +119,7 @@ end
 
 
 # Check build server load
-SCHEDULER.every '6s', :first_in => 0 do |job|
+SCHEDULER.every '5s', :first_in => 0 do |job|
   uri      = URI.parse(url)
   http     = Net::HTTP.new(uri.host, uri.port)
   api_url  = url + '/computer/api/json?tree=computer[displayName,idle]'
@@ -110,27 +135,18 @@ SCHEDULER.every '6s', :first_in => 0 do |job|
 end
 
 
-# Check since when the last buid failed and keep a record
-SCHEDULER.every '1s', :first_in => 0 do |job|
-  if is_any_build_failing
-    seconds_last_build_record = seconds_since_last_build_failed > seconds_last_build_record ? seconds_since_last_build_failed : seconds_last_build_record
-    seconds_since_last_build_failed = 0
-  else
-    seconds_since_last_build_failed += 1
-  end
-  send_event('jenkins_last_failed_since', { current: seconds_since_last_build_failed , last: seconds_last_build_record})
-end
-
-
+# Trim job names to avoid the long names break the frontend
 def trim_job_name(job_name)
-  job_name = job_name.gsub("grid-", "");
-  job_name = job_name.gsub("store-", "");
-  job_name = job_name.gsub("sphere-", "");
-  job_name = job_name.gsub("-public-deb", "");
-  job_name = job_name.gsub("-private-deb", "");
-  job_name = job_name.gsub("solr", "s");
-  job_name = job_name.gsub("automation", "am");
-  job_name = job_name.gsub("webtests-production", "wp");
-  job_name = job_name.gsub("webtests-staging", "ws");
+  job_name = job_name.gsub('grid-', '');
+  job_name = job_name.gsub('store-', '');
+  job_name = job_name.gsub('sphere-', '');
+  job_name = job_name.gsub('-public-deb', '');
+  job_name = job_name.gsub('-private-deb', '');
+  job_name = job_name.gsub('solr', 's');
+  job_name = job_name.gsub('automation', 'am');
+  job_name = job_name.gsub('webtests-production', 'wp');
+  job_name = job_name.gsub('webtests-staging', 'ws');
+  job_name = job_name.gsub('checkout', 'co');
+  job_name = job_name.gsub('saucelabs', 'slabs');
   return job_name
 end 
